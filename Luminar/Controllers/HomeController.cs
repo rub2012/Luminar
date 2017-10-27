3bbd3880e739d34e7aa3fcada2c82605ace83b54
Di Risio <julian.dirisio@externos-ar.cencosud.com>
2017-10-27 17:05:24 -0300

    manejo de api

--- Luminar/Controllers/HomeController.cs
+++ Luminar/Controllers/HomeController.cs
@@ -21,35 +21,7 @@ namespace Luminar.Controllers
 
         public IActionResult Nodo()
         {
-            var nodos = GetNodos();
-            var nodosActivos = new List<EnlaceDto>();
-            var red = (Dictionary<string,object>)NetJSON.NetJSON.DeserializeObject(new StreamReader("wwwroot/Data/nodos.json").ReadToEnd());
-            var coleccion = (IList<dynamic>)red["collection"];
-            var nodosJson = (IList<dynamic>)coleccion.Where(x => x["topology_id"] == "ipv4_0").Select(x => x["nodes"]).FirstOrDefault();
-            nodosJson = nodosJson.Where(x => (x["properties"]["type"] == "local" || x["properties"]["type"] == "node")).ToList();
-            var enlacesJson = (IList<dynamic>)coleccion.Where(x => x["topology_id"] == "ipv4_0").Select(x => x["links"]).FirstOrDefault();
-            enlacesJson = enlacesJson.Where(x => (x["properties"]["type"] == "local" || x["properties"]["type"] == "node")).ToList();
-            var nodosIp = nodos.Select(x => x.Ip).ToList();
-
-            foreach (var nodo in nodosJson)
-            {
-                ActivarNodo(nodos, (string)nodo["label"]);
-            };
-
-            foreach (var nodo in nodos)
-            {
-                var enlacesDelNodo = enlacesJson.Where(x => x["properties"]["source_addr"] == nodo.Ip).ToList();
-                nodo.Vecinos = new List<EnlaceDto>();
-                foreach (var enlace in enlacesDelNodo)
-                {
-                    nodo.Vecinos.Add(new EnlaceDto
-                    {
-                        Destino = enlace["properties"]["target_addr"],
-                        Costo = enlace["cost"]
-                    });
-                }
-
-            }
+            var nodos = ObtenerNodos();
 
             ViewBag.Nodos = nodos;
 
@@ -83,16 +55,44 @@ namespace Luminar.Controllers
 
         }
 
-        //private void Enlazar(NodoDto nodo, string nodoIp)
-        //{
-        //    foreach (var nodo in nodos)
-        //    {
-        //        if (nodo.Ip == nodoIp)
-        //        {
-        //            nodo.Activo = true;
-        //        }
-        //    };
+        private IList<NodoDto> ObtenerNodos()
+        {
+            var nodos = GetNodos();
+            var nodosActivos = new List<EnlaceDto>();
+            var red = (Dictionary<string, object>)NetJSON.NetJSON.DeserializeObject(new StreamReader("wwwroot/Data/nodos.json").ReadToEnd());
+            var coleccion = (IList<dynamic>)red["collection"];
+            var nodosJson = (IList<dynamic>)coleccion.Where(x => x["topology_id"] == "ipv4_0").Select(x => x["nodes"]).FirstOrDefault();
+            nodosJson = nodosJson.Where(x => (x["properties"]["type"] == "local" || x["properties"]["type"] == "node")).ToList();
+            var enlacesJson = (IList<dynamic>)coleccion.Where(x => x["topology_id"] == "ipv4_0").Select(x => x["links"]).FirstOrDefault();
+            enlacesJson = enlacesJson.Where(x => (x["properties"]["type"] == "local" || x["properties"]["type"] == "node")).ToList();
 
-        //}
+            foreach (var nodo in nodosJson)
+            {
+                ActivarNodo(nodos, (string)nodo["label"]);
+            };
+
+            foreach (var nodo in nodos)
+            {
+                var enlacesDelNodo = enlacesJson.Where(x => x["properties"]["source_addr"] == nodo.Ip).ToList();
+                nodo.Vecinos = new List<EnlaceDto>();
+                foreach (var enlace in enlacesDelNodo)
+                {
+                    nodo.Vecinos.Add(new EnlaceDto
+                    {
+                        Destino = enlace["properties"]["target_addr"],
+                        Costo = enlace["cost"]
+                    });
+                }
+            }
+            return nodos;
+        }
+    
+        [HttpGet]
+        public IActionResult NodosJson()
+        {
+            var nodos = ObtenerNodos();
+
+            return Json(nodos);
+        }
     }
 }
--- Luminar/wwwroot/js/grafico.js
+++ Luminar/wwwroot/js/grafico.js
@@ -1,5 +1,53 @@
 ﻿$(document).ready(function () {
 
     //var s = d3.
+    var bound = new google.maps.LatLngBounds();
+    $.getJSON($("#urlNodosJson").val(), function (data) {
+        var nodos = data;
+        $.each(nodos, function (indice, nodo) {
+            var latitud = nodo.latitude;
+            var longitud = nodo.latitude;
+            bound.extend(new google.maps.LatLng(latitud, longitud));
+        });
+
+        d3.selectAll("#map").attr("width", width - 200);
+
+        var latitudMax = Math.max.apply(Math, LatitudesNodos(nodos));
+        var latitudMin = Math.min.apply(Math, LatitudesNodos(nodos)); 
+
+        var longitudMax = Math.max.apply(Math, LongitudesNodos(nodos));
+        var longitudMin = Math.min.apply(Math, LongitudesNodos(nodos));
+
+        var map = new google.maps.Map(d3.select("#map").node(),
+            {
+                zoom: 1,
+                center: new google.maps.LatLng((latitudMax + latitudMin) / 2, (longitudMax + longitudMin) / 2),
+                mapTypeId: "roadmap"
+            });
+
+        map.fitBounds(bound);
+
+    });
+
 
 });
+
+function initMap() {
+
+}
+
+function LatitudesNodos(nodos) {
+    var latitudes = [];
+    $.each(nodos, function (indice, nodo) {
+        latitudes.push(nodo.latitud);
+    });
+    return latitudes;
+}
+
+function LongitudesNodos(nodos) {
+    var longitudes = [];
+    $.each(nodos, function (indice, nodo) {
+        longitudes.push(nodo.longitud);
+    });
+    return longitudes;
+}
